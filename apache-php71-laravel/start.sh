4ecf6b7e304d195482cbab05b4896448440b8ee5
Jhalak <onirudda.odikare@widespace.com>
2018-12-17 19:42:56 +0600

    Add PHP 7.1 Dockerfile

---
+++ apache-php71-laravel/start.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Configure Apache Document Root
+mkdir -p $APACHE_DOC_ROOT
+chown www-data:www-data $APACHE_DOC_ROOT
+sed -i "s|DocumentRoot /var/www/html\$|DocumentRoot $APACHE_DOC_ROOT/public|" /etc/apache2/sites-available/000-default.conf
+
+echo "<Directory $APACHE_DOC_ROOT/public>" > /etc/apache2/conf-available/document-root.conf
+echo "	AllowOverride All" >> /etc/apache2/conf-available/document-root.conf
+echo "	Require all granted" >> /etc/apache2/conf-available/document-root.conf
+echo "</Directory>" >> /etc/apache2/conf-available/document-root.conf
+a2enconf "document-root.conf"
+
+exec apache2 -DFOREGROUND
