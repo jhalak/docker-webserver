5227d2bbc0f6761869f8838aa61e508e1165e2b3
Jhalak <onirudda.odikare@widespace.com>
2019-06-17 11:41:02 +0600

    Update php70-laravel, php56-laravel and php72-general

--- apache-php56-laravel/start.sh
+++ apache-php56-laravel/start.sh
@@ -5,13 +5,13 @@ set -e
 # Configure Apache Document Root
 mkdir -p $APACHE_DOC_ROOT
 chown www-data:www-data $APACHE_DOC_ROOT
-sed -i "s|DocumentRoot /var/www/html\$|DocumentRoot $APACHE_DOC_ROOT|" /etc/apache2/sites-available/000-default.conf
+sed -i "s|DocumentRoot /var/www/html\$|DocumentRoot $APACHE_DOC_ROOT/public|" /etc/apache2/sites-available/000-default.conf
 
 echo "<Directory $APACHE_DOC_ROOT>" > /etc/apache2/conf-available/document-root.conf
-echo "	Options FollowSymLinks" >> /etc/apache2/conf-available/document-root.conf
 echo "	AllowOverride All" >> /etc/apache2/conf-available/document-root.conf
 echo "	Require all granted" >> /etc/apache2/conf-available/document-root.conf
 echo "</Directory>" >> /etc/apache2/conf-available/document-root.conf
 a2enconf "document-root.conf"
 
-exec apache2 -DFOREGROUND
+exec apache2-foreground
+service memcached start
